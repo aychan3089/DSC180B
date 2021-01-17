c4135d946c2acc00d5bc571c5c3bd7757d043113
Matthew Sao <msao@ucsd.edu>
2021-01-16 22:29:34 -0800

    Added a basic data pipeline that can get tweets, not all politicians included yet

---
+++ run.py
@@ -0,0 +1,18 @@
+import sys
+import os
+import json
+
+sys.path.insert(0, 'src/data')
+
+from data import lists
+
+def main(targets):
+
+    if 'data' in targets:
+        with open('config/data-params.json') as fh:
+            data_cfg = json.load(fh)
+        lists(**data_cfg)
+
+if __name__ == '__main__':
+    targets = sys.argv[1:]
+    main(targets)
