b1bcb4c9022fbed9d773c1d35ed7bc519134ac84
Matthew Sao <msao@ucsd.edu>
2021-01-26 20:48:34 -0800

    Created new data pipeline using txt files containing tweet ids

---
+++ src/data/data.py
@@ -0,0 +1,38 @@
+import tweepy
+import config
+# Authenticate twitter
+auth = tweepy.OAuthHandler(config.consumer_key, config.consumer_secret)
+auth.set_access_token(config.access_token, config.access_token_secret)
+api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+
+import time 
+import datetime
+import numpy as np
+import os
+import json
+
+def rehydrate(directory_path):
+    #Stores the current path so we can change back to it later
+    cwd = os.getcwd()
+    
+    #Changes the path to the folder containing the files we wish to rehydrate
+    os.chdir(directory_path)
+    
+    #Lists the files so we can iterate through them
+    folder = os.listdir()
+    
+    for file in range(len(folder)):
+        #Checks to make sure file ends in a txt extension 
+        if folder[file][-4:] == '.txt':
+            #Rehydrates the tweet ids using twarc
+            os.system('twarc hydrate ' + folder[file] + ' > ' + folder[file][:-4] + '.jsonl')
+            print("Data for " + folder[file][:-4] + " has been obtained.")
+        else:
+            continue
+            
+    #Changes the directory to the original path
+    os.chdir(cwd)
+        
+def get_data(scientific_path, misinformation_path):
+    rehydrate(scientific_path)
+    rehydrate(misinformation_path)
