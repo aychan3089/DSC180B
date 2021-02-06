bde98779831eecfa238dea64860c30554cacbc2c
Aaron Chan <aychan3089@gmail.com>
2021-02-06 00:49:42 -0800

    update metrics

--- src/lib/metrics.py
+++ src/lib/metrics.py
@@ -1,165 +1,186 @@
 import os 
 import json
-import ast
-import matplotlib.pyplot as plt
-
-s_path = '../src/data/scientific/'
-c_path = '../src/data/conspiracy/'
-
-
-# cumulative count
-# {name: {tweet number: total likes}}
-cumulative_likes_per_tweet = {}
-
-# {name: num tweets}
-num_tweets_per_account = {}
-
-for subdir, dirs, files in os.walk(s_path):
-    for file in files:
-        filepath = os.path.join(subdir, file)
-            
-        with open(filepath, encoding='utf-8') as f:
-            for line in f.readlines():
-                # tweet = json.loads(line)
-                tweet = ast.literal_eval(line)
-                
-                name = tweet['user']['name']
-                
-                if name not in cumulative_likes_per_tweet:
-                    cumulative_likes_per_tweet[name] = {}
-                    num_tweets_per_account[name] = 0
-                    
-                num_tweets_per_account[name] += 1
-                likes = tweet['favorite_count'] 
-                cumulative_likes = cumulative_likes_per_tweet.get(name).get(num_tweets_per_account[name] - 1, 0)
-                cumulative_likes_per_tweet[name][num_tweets_per_account[name]] = likes + cumulative_likes
-
-
-# running mean over cumulative tweets 
-# {name: {tweet_number: average likes last x tweets}}
-avg_likes_lastx_tweets = {}
-
-# {name: num tweets}
-num_tweets_per_account = {}
-
-all_likes = []
-x_tweets = 50
-
-for subdir, dirs, files in os.walk(s_path):
-    for file in files:
-        filepath = os.path.join(subdir, file)
-        
-        with open(filepath, encoding='utf-8') as f:
-            for line in f.readlines():
-                tweet = ast.literal_eval(line)
-                
-                name = tweet['user']['name']
-                
-                if name not in avg_likes_lastx_tweets:
-                    avg_likes_lastx_tweets[name] = {}
-                    num_tweets_per_account[name] = 0
-                    all_likes = []
-                
-                num_tweets_per_account[name] += 1
-                likes = tweet['favorite_count']
-                all_likes.append(likes)
-                
-                avg_likes = sum(all_likes[-x_tweets:]) // len(all_likes[-x_tweets:])
-                avg_likes_lastx_tweets[name][num_tweets_per_account[name]] = avg_likes
-
-
-# cumlative max 
-# {name: {tweet_number: cumulative max}}
-max_likes_per_tweet = {}
-
-# {name: num tweets}
-num_tweets_per_account = {}
-
-for subdir, dirs, files in os.walk(s_path):
-    for file in files:
-        filepath = os.path.join(subdir, file)
-        
-        with open(filepath, encoding='utf-8') as f:
-            for line in f.readlines():
-                tweet = ast.literal_eval(line)
-                
-                name = tweet['user']['name']
-                
-                if name not in max_likes_per_tweet:
-                    max_likes_per_tweet[name] = {}
-                    num_tweets_per_account[name] = 0
-                    
-                num_tweets_per_account[name] += 1
-                likes = tweet['favorite_count']
-
-                max_likes = max(likes, max_likes_per_tweet[name].get(num_tweets_per_account[name] - 1, 0))
-                max_likes_per_tweet[name][num_tweets_per_account[name]] = max_likes
-
-
-# rolling max 
-# {name: {tweet number: rolling max}}
-max_likes_lastx_tweets = {}
-
-# {name: num tweets}
-num_tweets_per_account = {}
-
-all_likes = []
-x_tweets = 50
-
-for subdir, dirs, files in os.walk(s_path):
-    for file in files:
-        filepath = os.path.join(subdir, file)
-        
-        with open(filepath, encoding='utf-8') as f:
-            for line in f.readlines():
-                tweet = ast.literal_eval(line)
-                
-                name = tweet['user']['name']
-                
-                if name not in max_likes_lastx_tweets:
-                    max_likes_lastx_tweets[name] = {}
-                    num_tweets_per_account[name] = 0
-                    
-                num_tweets_per_account[name] += 1
-                likes = tweet['favorite_count']
-                all_likes.append(likes)
-                
-                max_likes = max(all_likes[-x_tweets:])
-                max_likes_lastx_tweets[name][num_tweets_per_account[name]] = max_likes
-
 
 def convert_date(date):
-    created_at = tweet['created_at'].split()
+    created_at = date.split()
     month_conversion = {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
     return str(month_conversion[created_at[1]]) + '.' + created_at[-1]
 
+def make_months():
+    d = {}
+    for year_num in range(2008, 2021):
+        for month_num in range(1,13):
+            d[str(month_num) + '.' + str(year_num)] = 0
+    return d
 
-# cumulative count over time 
-# {name: {date: count}}
-likes_over_time = {}
 
-for subdir, dirs, files in os.walk(s_path):
-    for file in files:
-        filepath = os.path.join(subdir, file)
+def count_likes_over_months(path):
+    '''
+    returns 2 dictionaries of scientific users, misinformation users 
+    dictionary in the form of {name: {date: metric}}
+    '''
+    likes_over_time = {}
+
+    for subdir, dirs, files in os.walk(path):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+
+            with open(filepath, encoding='utf-8') as f:
+                for line in f.readlines():
+                    tweet = json.loads(line)
+
+                    name = tweet['user']['name']
+                    date = convert_date(tweet['created_at'])
+                    likes = tweet['favorite_count']
+
+                    if name not in likes_over_time:
+                        likes_over_time[name] = make_months()
+
+                    likes_over_time[name][date] += likes
+    return likes_over_time
+
+def avg_likes_over_months(path, x_months):
+    likes_over_time = count_likes_over_months(path)
+    avg_likes_over_time = {}
+    
+    for name in likes_over_time:
+        if name not in avg_likes_over_time:
+            avg_likes_over_time[name] = make_months()
         
-        with open(filepath, encoding='utf-8') as f:
-            for line in f.readlines():
-                tweet = ast.literal_eval(line)
-                
-                name = tweet['user']['name']
-                
-                if name not in likes_over_time:
-                    likes_over_time[name] = {}
+        months = list(likes_over_time[name].keys())
+        counts = list(likes_over_time[name].values())
+        
+        for i in range(len(months)): 
+            window = counts[max(i+1-x_months, 0): i+1]
+            avg_likes_over_time[name][months[i]] = sum(window) // len(window)
+    
+    return avg_likes_over_time
+
+def max_likes_over_months(path, x_months):
+    likes_over_time = count_likes_over_months(path)
+    max_likes_over_time = {}
+    
+    for name in likes_over_time:
+        if name not in max_likes_over_time:
+            max_likes_over_time[name] = make_months()
+        
+        months = list(likes_over_time[name].keys())
+        counts = list(likes_over_time[name].values())
+        
+        for i in range(len(months)): 
+            window = counts[max(i+1-x_months, 0): i+1]
+            max_likes_over_time[name][months[i]] = max(window)
+    
+    return max_likes_over_time
+
+def cumu_likes_over_months(path):
+    likes_over_time = count_likes_over_months(path)
+    cumu_likes_over_time = {}
+    
+    for name in likes_over_time:
+        if name not in cumu_likes_over_time:
+            cumu_likes_over_time[name] = make_months()
+        
+        total = 0
+        
+        for date in likes_over_time[name]:
+            total += likes_over_time[name][date]
+            cumu_likes_over_time[name][date] = total
+    
+    return cumu_likes_over_time
+
+def count_likes_over_tweets(path):
+    likes_per_tweet = {}
+    
+    for subdir, dis, files in os.walk(path):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            curr_num_tweets = 0
+            
+            with open(filepath, encoding='utf-8') as f:
+                for line in f.readlines():
+                    tweet = json.loads(line)
                     
-                date = convert_date(tweet['created_at'])
-                
-                likes_over_time[name][date] = likes_over_time[name].get(date, 0) + tweet['favorite_count']
+                    name = tweet['user']['name']
+                    likes = tweet['favorite_count'] 
+                    curr_num_tweets += 1
+                    
+                    if name not in likes_per_tweet:
+                        likes_per_tweet[name] = {}
+                        
+                    likes_per_tweet[name][curr_num_tweets] = likes
+    return likes_per_tweet
 
-# cumulative max over time 
-# {name: {date: max}}
+def avg_likes_over_tweets(path, x_tweets):
+    likes_per_tweet = {}
+    
+    for subdir, dis, files in os.walk(path):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            curr_num_tweets = 0
+            user_likes = []
+            
+            with open(filepath, encoding='utf-8') as f:
+                for line in f.readlines():
+                    tweet = json.loads(line)
+                    
+                    name = tweet['user']['name']
+                    likes = tweet['favorite_count'] 
+                    curr_num_tweets += 1
+                    user_likes.append(likes)
+                    
+                    if name not in likes_per_tweet:
+                        likes_per_tweet[name] = {}
+                    
+                    user_likes = user_likes[-x_tweets:]
+                    likes_per_tweet[name][curr_num_tweets] = sum(user_likes) // len(user_likes)
+    return likes_per_tweet
 
-# running mean over time 
-# {name: {date: rolling mean}}
+def max_likes_over_tweets(path, x_tweets):
+    likes_per_tweet = {}
+    
+    for subdir, dis, files in os.walk(path):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            curr_num_tweets = 0
+            user_likes = []
+            
+            with open(filepath, encoding='utf-8') as f:
+                for line in f.readlines():
+                    tweet = json.loads(line)
+                    
+                    name = tweet['user']['name']
+                    likes = tweet['favorite_count'] 
+                    curr_num_tweets += 1
+                    user_likes.append(likes)
+                    
+                    if name not in likes_per_tweet:
+                        likes_per_tweet[name] = {}
+                    
+                    user_likes = user_likes[-x_tweets:]
+                    likes_per_tweet[name][curr_num_tweets] = max(user_tweets)
+    return likes_per_tweet
 
-# running max over time
-# {name: {date: rolling max}}
+def cumu_likes_over_tweets(path):
+    likes_per_tweet = {}
+    
+    for subdir, dis, files in os.walk(path):
+        for file in files:
+            filepath = os.path.join(subdir, file)
+            curr_num_tweets = 0
+            total = 0
+            
+            with open(filepath, encoding='utf-8') as f:
+                for line in f.readlines():
+                    tweet = json.loads(line)
+                    
+                    name = tweet['user']['name']
+                    likes = tweet['favorite_count'] 
+                    curr_num_tweets += 1
+                    total += likes
+                    
+                    if name not in likes_per_tweet:
+                        likes_per_tweet[name] = {}
+                    
+                    likes_per_tweet[name][curr_num_tweets] = total
+    return likes_per_tweet
